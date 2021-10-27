d63d38d914dd12ed7bc33abe9fd75ec55b1569aa
Daniel Kim <kkdh1109@gmail.com>
2021-10-26 22:00:00 -0400

    hello world

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+//public keyword is an access modifier
+//modifiers allow user to define the scope
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
